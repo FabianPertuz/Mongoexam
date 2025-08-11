1e01ef5075570253c2f62f4b496b755a79932e38
FabianPertuz <fpertuz117.f@gmail.com>
2025-08-11 08:20:37 -0500

    feat: :sparkles: added consultas.js and music_store.json
    made all the nesesary code for the json and js file

---
+++ consultas.js
@@ -0,0 +1,10 @@
+// Mostrar albumes  del genero jazz
+db.album.find(genre{sin:["jazz"]})
+// mostrar albumes con precio mayor a 50000
+db.album.find(price{sin:[50000]})
+// mostrar albumes que no sean rock
+db.album.find(genre{sin:["jazz"]})
+// mostrar sku,title y tags con calificaion mayor a 4.5
+db.album.find(genre{sin:["jazz"]})
+// mostrar sku title y price de los albumes con stock menor a 5
+db.album.find(genre{sin:["jazz"]})
